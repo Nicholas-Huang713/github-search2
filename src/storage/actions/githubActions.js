8f0dc7858fc630e9baf9fb09d18e752fea939c82
Nicholas_Huang713 <nhuang713@gmail.com>
2020-11-15 05:34:44 -0800

    token removed

--- src/storage/actions/githubActions.js
+++ src/storage/actions/githubActions.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 export const getUser = (userName) => dispatch => {
     axios.get(`https://api.github.com/users/${userName}`, {
         auth: {
-            username: 'nicholas-huang713',
-            password: '59f7718421ff5ba6604e587ad8fe34cb6b36f2c1'
+            username: process.env.GITHUB_USERNAME,
+            password: process.env.TOKEN
         }
     })
     .then((res) => {
@@ -30,8 +30,8 @@ export const getUser = (userName) => dispatch => {
 export const getFollowing = (userName, pageSize) => dispatch => {
     axios.get(`https://api.github.com/users/${userName}/following?page=0&per_page=${pageSize}`, {
         auth: {
-            username: 'nicholas-huang713',
-            password: '59f7718421ff5ba6604e587ad8fe34cb6b36f2c1'
+            username: process.env.GITHUB_USERNAME,
+            password: process.env.TOKEN
         }
     })
     .then((res) => {
